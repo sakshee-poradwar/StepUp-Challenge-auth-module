89b4c4c9332171f9e0030942467b1c0c455781b6
sakshee-poradwar <poradwarsakshee21@gmail.com>
2026-08-15 19:23:45 +0530

    Create sign_out.dart

---
+++ sign_out.dart
@@ -0,0 +1,7 @@
+import 'package:stepauth/data/repositories/auth_repository.dart';
+
+class SignOut {
+  const SignOut(this._repository);
+  final AuthRepository _repository;
+  Future<void> call() => _repository.signOut();
+}
